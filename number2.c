8675078edd17921dd0ce00cc2c673a46856846ba
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-01-19 16:47:42 +0530

    Create number2.c

---
+++ number2.c
@@ -0,0 +1,26 @@
+int main()
+{
+    int num;
+    int tempNum,flag;
+     
+    printf("Enter an integer number: ");
+    scanf("%d",&num);
+     
+    tempNum=num;
+    flag=0;
+    while(tempNum!=1)
+    {
+        if(tempNum%2!=0){
+            flag=1;
+            break;
+        }
+        tempNum=tempNum/2;
+    }
+  
+    if(flag==0)
+        printf("%d is a number that is the power of 2.",num);
+    else
+        printf("%d is not the power of 2.",num);
+      
+    return 0;
+}
