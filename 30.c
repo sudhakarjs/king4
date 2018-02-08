d7f505e0b45458a7022f67460d6ea85dbee84eb2
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 09:25:43 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define MAX_SIZE1000
+void main()
+{
+int arr[MAX_SIZE];
+int a,n;
+printf("enter size of  array:");
+scanf("%d",&n);
+printf("enter %d elements in the array:" ,n);
+for(a=0;a<n;a++)
+{
+scanf("%d",&arr[a])
+}
+printf("\n elements in array are:");
+for(a=0;a<n;a++)
+{
+printf("%d",array[a]);
+}
+getch();
+}
