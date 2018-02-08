e0965b3cfa49034894fee9dbd7fb1dc96cfc7f28
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 09:24:04 +0000

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char ab[25];
+printf("\n enter the string:");
+scanf("%s",ab);
+if(a>o)
+ printf("\n yes");
+else
+ printf("\n no");
+getch();
+}
